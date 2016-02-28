e989f7150581a4b833443f589f1c09cf8811fa96
cbinnie <cbinnie.sfu.ca>
2016-02-28 00:03:53 -0800

    Some camara and player movements

---
+++ UHack/Assets/Script/Camera_Movement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera_Movement : MonoBehaviour
+{
+	public GameObject player;
+	// Use this for initialization
+	void Start ()
+	{
+		transform.position = new Vector3(0, 6, 0);
+		transform.rotation = Quaternion.Euler(90, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 6, player.transform.position.z);
+	}
+}
---
+++ UHack/Assets/Script/Move.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour
+{
+	public float speed = 5;
+	
+	//When the player hits an object
+	void OnCollisionEnter (Collision col)
+    {
+        if (col.gameObject.name == "Sphere")
+        {
+            Destroy(col.gameObject);
+        }
+    }
+	
+	// Use this for initialization
+	void Start ()
+	{
+		transform.position = new Vector3(0, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.Translate(Vector3.right * speed * Input.GetAxis("Horizontal") * Time.deltaTime);
+		transform.Translate(Vector3.forward * speed * Input.GetAxis("Vertical") * Time.deltaTime);
+	}
+}
